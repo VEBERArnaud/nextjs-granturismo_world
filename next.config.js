47b427c60ebd06ec8fd4e6193bf29aced915e6ba
VEBER Arnaud <arnaud@veber.pw>
2019-02-14 15:07:15 +0100

    feat(next): asset prefix

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
+const assetPrefix = process.env.ASSET_PREFIX || '';
+
 module.exports = {
   target: 'serverless',
+  assetPrefix: assetPrefix,
+  env: {
+    ASSET_PREFIX: assetPrefix,
+  },
   webpack: config => {
     // Fixes npm packages that depend on `fs` module
     config.node = {
